5460e158d20f30e51e8ed10fc9816119afdef346
Valdir <valdircaf@hotmail.com>
2025-09-27 23:42:31 -0300

    transcriptions works

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const { exec } = require("child_process");
+const { exec, spawn } = require("child_process");
+const { stderr, stdout } = require('process');
+const { error } = require('console');
+const path = require('path');
 
 const hostname = "localhost";
 const port = 3000;
@@ -18,37 +21,35 @@ app.get("/", (req, res) => {
 
 app.post("/parseYtUrl", (req, res) => {
     const url = req.body.url;
-    // exec(`yt-dlp -x --audio-format mp3 "${url}" --extractor-args "youtube:player-client=default,-tv_simply"`, (err, stdout, stderr) => {
-    //     if (err) {
-    //         res.send(err.message)
-    //         return;
-    //     }
-    //     if (stderr) {
-    //         res.send(stderr)
-    //         return;
-    //     }
-
-    exec(`ffmpeg -i "Java no Visual Studio Code： Projetos com Spring Boot [dkmlOi_MNb4].mp3" -ar 16000 -ac 1 -c:a pcm_s16le output.wav`, (err, stdout, stderr) => {
+    const actDir = process.cwd() + "/server"
+    exec(`yt-dlp -x --audio-format mp3 -o "${actDir}/%(id)s.%(ext)s" --print filename "${url}" --extractor-args "youtube:player-client=default,-tv_simply"`, (err, stdout, stderr) => {
         if (err) {
             res.send(err.message)
             return;
         }
-        if (stderr) {
-            res.send(stderr)
-            return;
-        }
-        exec(`../whisper.cpp/build/bin/whisper-cli -f "output.wav -otxt"`, (err, stdout, stderr) => {
-            if (err) {
-                res.send(err.message)
-                return;
-            }
-            if (stderr) {
-                res.send(stderr)
-                return;
-            }
-            res.send(stdout)
-        });
-    })
+        console.log(stdout.trim());
+        const fullPath = stdout.trim();
+        const name = path.parse(fullPath).name.replace("webm", "mp3");
+        const fileName = name + ".mp3";
+        const wavFile = name + ".wav";
+        exec(`ffmpeg -i "${fileName}" -ar 16000 -ac 1 -c:a pcm_s16le "${wavFile}"`, 
 
-});
-// })
+            {cwd: "/Users/novo/Documents/projetos/transcribe-ytb/server"},
+            (error, stdout, stderr) => {
+            if (error) {
+                res.send(error.message)
+                return;
+            }
+                exec(`../whisper.cpp/build/bin/whisper-cli -f "${wavFile}" -m ../whisper.cpp/models/ggml-base.bin -otxt -l pt`, 
+                {cwd: "/Users/novo/Documents/projetos/transcribe-ytb/server"},
+                (error, stdout, stderr) => {
+                if (error) {
+                    res.send(error.message)
+                    return;
+                }
+                
+                res.send(stdout)
+        })
+    }) 
+})
+})
